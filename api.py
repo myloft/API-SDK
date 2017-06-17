f05c150be923fbf186805c97e83943a205ddc8be
myloft <admin@iloft.xyz>
2017-06-17 11:41:48 +0800

    SDK v0.2
    合并API结构,简化SDK使用

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-"""API Script"""
+"""API SDK"""
 # coding:utf-8
 
 import requests
@@ -11,81 +11,58 @@ HEADERS = {'content-type': 'application/json'}
 def get_status(type_id, device_id):
     """Get Device Status"""
     if type_id == 0:
-        g_emitter = requests.get(
-            url=URL + 'emitter/' + str(device_id), auth=AUTH)
-        emitter_data = g_emitter.json()
-        return emitter_data['status']
+        g_controller = requests.get(
+            url=URL + 'controller/' + str(device_id), auth=AUTH)
+        controller_data = g_controller.json()
+        return controller_data['status']
     elif type_id == 1:
-        g_relay = requests.get(url=URL + 'relay/' + str(device_id), auth=AUTH)
-        relay_data = g_relay.json()
-        return relay_data['status']
-    elif type_id == 2:
         g_sensor = requests.get(url=URL + 'sensor/' +
                                 str(device_id), auth=AUTH)
         sensor_data = g_sensor.json()
         return sensor_data['status']
 
 
-def patch_status(type_id, device_id, data):
+def patch_status(type_id, device_id, status):
     """Edit Device Status"""
     if type_id == 0:
-        p_emitter = requests.patch(url=URL + 'emitter/' + str(device_id),
-                                   data='{"status": ' + str(data) + '}', headers=HEADERS, auth=AUTH)
-        return p_emitter.status_code
+        p_status = requests.patch(url=URL + 'controller/' + str(device_id),
+                                  data='{"status": ' + str(status) + '}', headers=HEADERS, auth=AUTH
+                                 )
+        return p_status.status_code
     elif type_id == 1:
-        p_relay = requests.patch(url=URL + 'relay/' + str(device_id),
-                                 data='{"status": ' + str(data) + '}', headers=HEADERS, auth=AUTH)
-        return p_relay.status_code
-    elif type_id == 2:
-        p_sensor = requests.patch(url=URL + 'sensor/' + str(device_id),
-                                  data='{"status": ' + str(data) + '}', headers=HEADERS, auth=AUTH)
-        return p_sensor.status_code
+        p_status = requests.patch(url=URL + 'sensor/' + str(device_id),
+                                  data='{"status": ' + str(status) + '}', headers=HEADERS, auth=AUTH
+                                 )
+        return p_status.status_code
 
 
-def create_emitter(function, data):
-    """Create New Emitter"""
-    c_emitter = requests.post(url=URL + 'emitter/', data='{"function":' + '"' + str(
-        function) + '"' + ',' + '"status":' + str(data) + '}', headers=HEADERS, auth=AUTH)
-    if c_emitter.status_code == 201:
-        emitter_data = c_emitter.json()
-        return emitter_data['id']
-    else:
-        print("创建失败,请检查是否又重复设备")
-
-
-def create_relay(data):
-    """Create New Relay"""
-    c_relay = requests.post(
-        url=URL + 'relay/', data='{"status": ' + str(data) + '}', headers=HEADERS, auth=AUTH)
-    if c_relay.status_code == 201:
-        relay_data = c_relay.json()
-        return relay_data['id']
-    else:
-        print("创建失败,请检查是否又重复设备")
-
-
-def create_sensor(name, data):
-    """Create New Sensor"""
-    c_sensor = requests.post(url=URL + 'sensor/', data='{"name":' + '"' + str(
-        name) + '"' + ',' + '"status":' + str(data) + '}', headers=HEADERS, auth=AUTH)
-    if c_sensor.status_code == 201:
-        sensor_data = c_sensor.json()
-        return sensor_data['id']
-    else:
-        print("创建失败,请检查是否又重复设备")
+def create_device(type_id, name, status):
+    """Create New device"""
+    if type_id == 0:
+        c_device = requests.post(url=URL + 'controller/', data='{"name":' + '"' + str(
+            name) + '"' + ',' + '"status":' + str(status) + '}', headers=HEADERS, auth=AUTH)
+        if c_device.status_code == 201:
+            device_data = c_device.json()
+            return device_data['id']
+        else:
+            print("创建失败,请检查是否有重名设备")
+    if type_id == 1:
+        c_device = requests.post(url=URL + 'sensor/', data='{"name":' + '"' + str(
+            name) + '"' + ',' + '"status":' + str(status) + '}', headers=HEADERS, auth=AUTH)
+        if c_device.status_code == 201:
+            device_data = c_device.json()
+            return device_data['id']
+        else:
+            print("创建失败,请检查是否有重名设备")
 
 
 def delete_device(type_id, device_id):
     """Delete Device"""
     if type_id == 0:
         d_device = requests.delete(
-            url=URL + 'emitter/' + str(device_id), headers=HEADERS, auth=AUTH)
+            url=URL + 'controller/' + str(device_id), headers=HEADERS, auth=AUTH)
         return d_device.status_code
     if type_id == 1:
-        d_device = requests.delete(
-            url=URL + 'relay/' + str(device_id), headers=HEADERS, auth=AUTH)
-        return d_device.status_code
-    if type_id == 2:
         d_device = requests.delete(
             url=URL + 'sensor/' + str(device_id), headers=HEADERS, auth=AUTH)
         return d_device.status_code
--- demo.py
+++ demo.py
@@ -1,21 +1,20 @@
-"""TEST API"""
+"""API SDK Demo"""
 # coding:utf-8
-from api import get_status, patch_status, create_emitter, create_relay, create_sensor, delete_device
+from api import get_status, patch_status, create_device, delete_device
 
 
-# 0:emitter 红外发射器 (0,1):温度 (0,2):模式 (0,3):风速 (0,4):电源
-# 1:relay 继电器 (1,1):继电器1状态 (1,2):继电器2状态
-# 2:sensor 传感器 (2,1):雨水状态 (2,2):红外状态 (2,3):门磁状态 (2,4):PM2.5状态 (2,5):温度状态
-# 访问 http://api.iloft.xyz/admin 具体查看设备列表
+# 0:controller 控制器 (0,0):继电器0 (0,1):继电器1 (0,2):空调电源 (0,3):空调模式 (0,4):空调温度 (0,5):空调风速
+# 1:sensor 传感器 (1,0):雨水传感器 (1,1):PM2.5传感器 (1,2):温度传感器
+# 访问 http://api.iloft.xyz/admin 查看设备最新列表或手动管理设备
 
-print(patch_status(2, 5, 29))  # 推送温度传感器状态并返回状态码 返回200成功
+patch_status(1, 2, 29)  # 推送温度传感器状态并返回状态码 返回200成功
 
-TMP = get_status(2, 5)  # 读取温度传感器状态
+TMP = get_status(1, 2)  # 读取温度传感器状态
 
 print(TMP)  # 显示获取到的温度
 
-ID = create_emitter("test", 0)  # 创建新的红外发射功能test 初始值为0 返回设备device_id
+ID = create_device(0, "test", 0)  # 创建新的控制器test 初始值为0 返回设备id
 
-print(ID)  # 显示创建的设备device_id
+print(ID)  # 显示新创建的设备id
 
-print(delete_device(0, ID))  # 删除刚创建的设备 返回204成功
+delete_device(0, ID)  # 删除刚创建的设备 返回204成功
